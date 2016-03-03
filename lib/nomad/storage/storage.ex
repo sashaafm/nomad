26060caa45b6cbf61961a00ee141e3cc91afac4f
Sasha Fonseca <sashaafm@gmail.com>
2016-03-03 17:38:58 +0000

    Continue testing.

--- lib/nomad/storage/storage.ex
+++ lib/nomad/storage/storage.ex
@@ -1,7 +1,20 @@
 defmodule Nomad.Storage do
+  alias Cloud.Storage, as: CS
 
-  def insert do 
-    Cloud.Storage.insert 
+  def list_storages do 
+    CS.list_storages
+  end
+
+  def create_storage(name, region) do 
+    CS.create_bucket name, region
+  end
+
+  def put_item(storage, name, content) do 
+    CS.put_item storage, name, content
+  end
+
+  def list_items(storage) do 
+    CS.list_items storage
   end
   
 end
