b5a84540656049501e98f6a17286756ac2435c16
sashaafm <sashaafm@gmail.com>
2016-03-14 21:16:18 +0000

    Changed the config to only use the configs needed for the current provider.

--- config/dev.exs
+++ config/dev.exs
@@ -5,12 +5,18 @@ config :nomad,
   target_host: System.get_env("HOST"),
   target_port: System.get_env("PORT")
 
-config :nomad_gcl,
-  storage_scope: "https://www.googleapis.com/auth/devstorage.full_control"
+case System.get_env("PROVIDER") do
+  "GCL" ->
+    config :nomad_gcl,
+      storage_scope: "https://www.googleapis.com/auth/devstorage.full_control"
 
- config :goth, 
- 	json: "config/creds.json" |> Path.expand |> File.read!
+     config :goth, 
+     	json: "config/creds.json" |> Path.expand |> File.read!
 
-config :gcloudex,
-  storage_scope: "https://www.googleapis.com/auth/devstorage.full_control",
-  storage_proj:  "330132837690"  
+    config :gcloudex,
+      storage_scope: "https://www.googleapis.com/auth/devstorage.full_control",
+      storage_proj:  "330132837690"  
+
+  "AWS" ->
+    :ok
+end
