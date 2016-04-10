208d3433034442a58b4ae8b19f041237a1f01009
sashaafm <sashaafm@gmail.com>
2016-04-10 11:48:35 +0100

    Added documentation and specs for the Nomad.SQL module's functions.

--- lib/nomad/sql/sql.ex
+++ lib/nomad/sql/sql.ex
@@ -8,30 +8,64 @@ defmodule Nomad.SQL do
 
   @behaviour NomadSQL
 
+  @doc """
+  Lists all database instances for the given cloud credentials. The function
+  returns a list of tuples with the following format:
+    {instance_name, region, address, status, allocated_storage}
+  """
+  @spec list_instances() :: [tuple]
   def list_instances do
     CS.list_instances
   end
   
+  @doc """
+  Returns a tuple with info about the given 'instance' with the format:
+    {instance_name, region, address, status, allocated_storage}
+  """
+  @spec get_instance(binary) :: tuple
   def get_instance(instance) do 
     CS.get_instance instance
   end
 
+  @doc """
+  Creates a new database instance with name 'instance', the provided 
+  'settings' and in the desired pricing/performance 'tier'.
+
+  The 'settings' must be passed as a Map in the format: %{key: value}
+  """
+  @spec insert_instance(binary, map, binary) :: :ok | binary
   def insert_instance(instance, settings, tier) do 
     CS.insert_instance instance, settings, tier
   end
 
+  @doc """
+  Deletes the given 'instance'.
+  """
+  @spec delete_instance(binary) :: :ok | binary
   def delete_instance(instance) do 
     CS.delete_instance instance
   end
 
+  @doc """
+  Restarts the given 'instance'.
+  """
+  @spec restart_instance(binary) :: :ok | binary
   def restart_instance(instance) do 
     CS.restart_instance instance
   end
 
+  @doc """
+  Lists all the available databases in the given 'instance'.
+  """
+  @spec list_databases(binary) :: [binary]
   def list_databases(instance) do 
     CS.list_databases instance
   end
 
+  @doc """
+  Lists all instance classes/tiers for the given cloud provier's SQL service.
+  """
+  @spec list_classes() :: [binary]
   def list_classes do 
     CS.list_classes
   end
