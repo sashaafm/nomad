3c6570c219ede203b6e1fab0e389d461e2a9cd60
Sasha Fonseca <sashaafm@gmail.com>
2016-04-12 14:54:25 +0100

    Added a new module Nomad.Utils for auxiliary functions and a find_public_ip_address/0 function to retrieve the current machine's public IP.

---
+++ lib/nomad/utils.ex
@@ -0,0 +1,19 @@
+defmodule Nomad.Utils do
+  alias HTTPoison, as: HTTP
+  alias Poison,   as: JSON
+
+  @moduledoc """
+  Offers auxiliary functions for Nomad workflows.
+  """
+
+  @doc """
+  Returns the current machine's public IP address. An HTTP call is made to the
+  ipecho.net service and the reply is parsed. If this service goes down in the
+  future another one will have to take it's place.
+  """
+  @spec find_public_ip_address() :: binary
+  def find_public_ip_address do
+    {:ok, res} = HTTP.request(:get, "http://ipecho.net/plain", "", [], [])
+    res.body
+  end
+end
