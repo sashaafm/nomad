eb2a435f30451b59a4ea258a64984b7428c5fc77
Sasha Fonseca <sashaafm@gmail.com>
2016-04-19 17:50:56 +0100

    Mix task for listing database instances now pretty prints :).

--- lib/mix/tasks/sql/database_instance_list.ex
+++ lib/mix/tasks/sql/database_instance_list.ex
@@ -43,64 +43,23 @@ defmodule Mix.Tasks.Nomad.DatabaseInstance.List do
     res = Nomad.SQL.list_instances
 
     if is_list(res) do 
-      columnar = res
+      res = res
       |> convert_instance_tuples_to_lists
       |> stringify
-      |> transpose
 
-      {max_name, max_region, max_addr, max_status, max_sto} = columnar
-      |> get_maximum_data_length
-      |> List.to_tuple
-
-      name_h    = String.ljust("Name",    max_name   - String.length("name"))
-      region_h  = String.ljust("Region",  max_region - String.length("region"))
-      addr_h    = String.ljust("Address", max_addr   - String.length("address"))
-      status_h  = String.ljust("Status",  max_status - String.length("status"))
-      storage_h = String.ljust("Storage", max_sto    - String.length("storage"))
-
-      res = ljust_all_entries columnar, {max_name, max_region, max_addr, max_status, max_sto}
-
-      # TODO PRETTY PRINT THE RESULTS
+      TableRex.quick_render!(
+        res, 
+        ["Name", "Region", "Address", "Status", "Storage"],
+        "List of SQL Database Instances on #{System.get_env("PROVIDER")}")
+      |> Mix.Shell.IO.info
     else
       Mix.Shell.IO.info("There was a problem listing the instances:\n#{res}")
     end
   end
 
-  defp ljust_all_entries([names, regions, addresses, statuses, storages],
-                         {mname, mregion, maddress, mstatus, mstorage}) do 
-    new_names   = Enum.map(["Name"]    ++ names,     fn x -> ljust(x, mname    - String.length(x)) end)
-    new_region  = Enum.map(["Region"]  ++ regions,   fn x -> ljust(x, mregion  - String.length(x)) end)    
-    new_addr    = Enum.map(["Address"] ++ addresses, fn x -> ljust(x, maddress - String.length(x)) end)    
-    new_status  = Enum.map(["Status"]  ++ statuses,  fn x -> ljust(x, mstatus  - String.length(x)) end)        
-    new_storage = Enum.map(["Storage"] ++ storages,  fn x -> ljust(x, mstorage - String.length(x)) end)        
-
-    new_names ++ new_region ++ new_addr ++ new_status ++ new_storage
-  end
-
-  defp ljust(str, val), do: String.ljust(str, val)
-
-  defp get_maximum_data_length(instances) do 
-    instances
-    |> get_maximum_elements_from_each_data_list
-    |> Enum.map(&String.length/1)
-  end
-
   defp convert_instance_tuples_to_lists(instances) do
     instances 
     |> Enum.map(fn instance -> Tuple.to_list(instance) end)    
-  end
-
-  defp get_maximum_elements_from_each_data_list(data_lists) do 
-    data_lists
-    |> Enum.map(fn data_list -> 
-                 data_list
-                 |> Enum.max_by(fn data_elem -> String.length(data_elem) end)
-                end)    
-  end
-
-  defp transpose([[]|_]), do: []
-  defp transpose(rows) do
-    [Enum.map(rows, &hd/1) | transpose(Enum.map(rows, &tl/1))]
   end
 
   defp stringify(rows) do
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nomad.Mixfile do
   def project do
     [
      app:             :nomad,
-     version:         "0.1.0",
+     version:         "0.4.0",
      elixir:          "~> 1.2",
      build_embedded:  Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -16,13 +16,13 @@ defmodule Nomad.Mixfile do
     case System.get_env("PROVIDER") do 
       "AWS" ->
         [
-          applications: [:logger, :httpoison, :nomad_aws],
+          applications: [:logger, :httpoison, :nomad_aws, :table_rex],
           mod: {Nomad, []}
         ]
 
       "GCL" ->
         [
-          applications: [:logger, :httpoison, :nomad_gcl],
+          applications: [:logger, :httpoison, :nomad_gcl, :table_rex],
           mod: {Nomad, []}
         ]
     end
@@ -32,23 +32,25 @@ defmodule Nomad.Mixfile do
     case System.get_env("PROVIDER") do 
       "AWS" ->
         [
-          {:exrm,      "~> 1.0.1"},
-          {:credo,     "~> 0.3",  only: [:dev, :test]},
-          {:ex_doc,    "~> 0.11", only: [:dev]},
-          {:httpoison, "~> 0.8.1"},
+          {:exrm,             "~> 1.0.1"},
+          {:credo,            "~> 0.3",  only: [:dev, :test]},
+          {:ex_doc,           "~> 0.11", only: [:dev]},
+          {:httpoison,        "~> 0.8.1"},
+          {:table_rex,        "~> 0.8.0"},
          # {:nomad_aws, git: "git@github.com:sashaafm/nomad_aws.git"}
-          {:nomad_aws, path: "/home/sashaafm/Documents/nomad_aws"},
+          {:nomad_aws,        path: "/home/sashaafm/Documents/nomad_aws"},
           {:nomad_behaviours, path: "/home/sashaafm/Documents/nomad_behaviours"}
         ]
 
       "GCL" ->
         [
-          {:exrm,      "~> 1.0.1"},
-          {:credo,     "~> 0.3",  only: [:dev, :test]},
-          {:ex_doc,    "~> 0.11", only: [:dev]},
-          {:httpoison, "~> 0.8.1"},
+          {:exrm,             "~> 1.0.1"},
+          {:credo,            "~> 0.3",  only: [:dev, :test]},
+          {:ex_doc,           "~> 0.11", only: [:dev]},
+          {:httpoison,        "~> 0.8.1"},
+          {:table_rex,        "~> 0.8.0"},
         #  {:nomad_gcl, git: "git@github.com:sashaafm/nomad_gcl.git"}
-          {:nomad_gcl, path: "/home/sashaafm/Documents/nomad_gcl"},
+          {:nomad_gcl,        path: "/home/sashaafm/Documents/nomad_gcl"},
           {:nomad_behaviours, path: "/home/sashaafm/Documents/nomad_behaviours"}
         ]        
     end    
