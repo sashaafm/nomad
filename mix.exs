b1da1a0be08bb1167855fc0ccd64075498f7586a
Sasha Fonseca <sashaafm@gmail.com>
2016-05-15 15:45:24 +0100

    Version bump.

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Nomad.Mixfile do
   use Mix.Project
 
+  @version
+
   def project do
     [
      app:             :nomad,
-     version:         "0.4.0",
+     version:         "0.5.0",
      elixir:          "~> 1.2",
      build_embedded:  Mix.env == :prod,
      start_permanent: Mix.env == :prod,
