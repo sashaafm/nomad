7dab5870a75f47510491ae5934d3e72777cb0bf1
Sasha Fonseca <sashaafm@gmail.com>
2016-05-24 17:53:49 +0100

    Added the create_disk function for GCL.

--- lib/nomad/adapters/gcl/virtual_machines/gcl_virtual_machines.ex
+++ lib/nomad/adapters/gcl/virtual_machines/gcl_virtual_machines.ex
@@ -1,4 +1,4 @@
-if Code.ensure_loaded?(GCloudex) do 
+if Code.ensure_loaded?(GCloudex) do
   defmodule Nomad.GCL.VirtualMachines do
 
     @moduledoc """
@@ -6,8 +6,8 @@ if Code.ensure_loaded?(GCloudex) do
     GCloudex.
     """
 
-    defmacro __using__(:gcl) do 
-      quote do 
+    defmacro __using__(:gcl) do
+      quote do
         import GCloudex.ComputeEngine.Client
         import Nomad.Utils
 
@@ -15,12 +15,12 @@ if Code.ensure_loaded?(GCloudex) do
         ### Virtual Machines ###
         ########################
 
-        def list_virtual_machines(region, fun \\ &list_instances/2) do 
+        def list_virtual_machines(region, fun \\ &list_instances/2) do
           query = %{"fields" => "items"}
           case fun.(region, query) do
             {:ok, res} ->
-              case res.status_code do 
-                200 -> 
+              case res.status_code do
+                200 ->
                   res.body
                   |> Poison.decode!
                   |> Map.get("items")
@@ -34,16 +34,16 @@ if Code.ensure_loaded?(GCloudex) do
           end
         end
 
-        defp get_vm_data(vm) do 
-          ip = vm["networkInterfaces"] 
-          |> List.first 
-          |> Map.get("accessConfigs") 
-          |> List.first 
+        defp get_vm_data(vm) do
+          ip = vm["networkInterfaces"]
+          |> List.first
+          |> Map.get("accessConfigs")
+          |> List.first
           |> Map.get("natIP")
 
           {
-            vm["name"], 
-            vm["status"], 
+            vm["name"],
+            vm["status"],
             vm["machineType"] |> String.split("/") |> List.last,
             if ip != nil do ip else "No Address" end
           }
@@ -64,19 +64,19 @@ if Code.ensure_loaded?(GCloudex) do
             {:ok, res} ->
               case res.status_code do
                 200 -> :ok
-                _   -> res |> show_error_message_and_code(:json) 
+                _   -> res |> show_error_message_and_code(:json)
               end
             {:error, reason} ->
               parse_http_error reason
-          end 
-        end 
+          end
+        end
 
 
-        def get_virtual_machine(region, instance, fun \\ &get_instance/3) do 
+        def get_virtual_machine(region, instance, fun \\ &get_instance/3) do
           fields = "machineType, networkInterfaces, status"
-          case fun.(region, instance, fields) do 
+          case fun.(region, instance, fields) do
             {:ok, res} ->
-              case res.status_code do 
+              case res.status_code do
                 200 ->
                   res.body
                   |> Poison.decode!
@@ -91,41 +91,17 @@ if Code.ensure_loaded?(GCloudex) do
         end
 
         defp gvm(body, instance, region) do
-          ip = body["networkInterfaces"] 
-          |> List.first 
-          |> Map.get("accessConfigs") 
-          |> List.first 
+          ip = body["networkInterfaces"]
+          |> List.first
+          |> Map.get("accessConfigs")
+          |> List.first
           |> Map.get("natIP")
 
           {instance, body["status"], region, ip}
         end
 
-        def delete_virtual_machine(region, instance, fun \\ &delete_instance/2) do 
-          case fun.(region, instance) do 
-            {:ok, res} ->
-              case res.status_code do 
-                200 -> :ok
-                _   -> res |> show_error_message_and_code(:json)
-              end
-            {:error, reason} ->
-              parse_http_error reason
-          end
-        end
-
-        def start_virtual_machine(region, instance, fun \\ &start_instance/2) do 
-          case fun.(region, instance) do 
-            {:ok, res} ->
-              case res.status_code do 
-                200 -> :ok
-                _   -> res |> show_error_message_and_code(:json)
-              end
-            {:error, reason} ->
-              parse_http_error reason
-          end
-        end
-
-        def stop_virtual_machine(region, instance, fun \\ &stop_instance/2) do 
-          case fun.(region, instance) do 
+        def delete_virtual_machine(region, instance, fun \\ &delete_instance/2) do
+          case fun.(region, instance) do
             {:ok, res} ->
               case res.status_code do
                 200 -> :ok
@@ -136,10 +112,10 @@ if Code.ensure_loaded?(GCloudex) do
           end
         end
 
-        def reboot_virtual_machine(region, instance, fun \\ &reset_instance/2) do 
-          case fun.(region, instance) do 
+        def start_virtual_machine(region, instance, fun \\ &start_instance/2) do
+          case fun.(region, instance) do
             {:ok, res} ->
-              case res.status_code do 
+              case res.status_code do
                 200 -> :ok
                 _   -> res |> show_error_message_and_code(:json)
               end
@@ -148,8 +124,32 @@ if Code.ensure_loaded?(GCloudex) do
           end
         end
 
-        def set_virtual_machine_class(region, instance, class, fun \\ &set_machine_type/3) do 
-          case fun.(region, instance, class) do 
+        def stop_virtual_machine(region, instance, fun \\ &stop_instance/2) do
+          case fun.(region, instance) do
+            {:ok, res} ->
+              case res.status_code do
+                200 -> :ok
+                _   -> res |> show_error_message_and_code(:json)
+              end
+            {:error, reason} ->
+              parse_http_error reason
+          end
+        end
+
+        def reboot_virtual_machine(region, instance, fun \\ &reset_instance/2) do
+          case fun.(region, instance) do
+            {:ok, res} ->
+              case res.status_code do
+                200 -> :ok
+                _   -> res |> show_error_message_and_code(:json)
+              end
+            {:error, reason} ->
+              parse_http_error reason
+          end
+        end
+
+        def set_virtual_machine_class(region, instance, class, fun \\ &set_machine_type/3) do
+          case fun.(region, instance, class) do
             {:ok, res} ->
               case res.status_code do
                 200 -> :ok
@@ -213,7 +213,45 @@ if Code.ensure_loaded?(GCloudex) do
           {disk["name"], disk["sizeGb"], image, disk["status"], type}
         end
 
-        #!!!!!!!!!!!!!!!!!!!!!! INSERT DISK MISSING !!!!!!!!!!!!!!!!!!!!!!!!!!!#
+        def create_disk(region, name, size) do
+          cd region, name, size  
+        end
+
+        def create_disk(region, name, size, image) do
+          cd_with_img region, name, size, image  
+        end
+
+        defp cd(region, size, fun \\ &GCloudex.ComputeEngine.Client.insert_disk/2) do
+          epoch    = {{1970, 1, 1}, {0, 0, 0}} |> :calendar.datetime_to_gregorian_seconds
+          name     = "disk-#{epoch}" 
+          resource = %{"name" => name, "sizeGb" => size}
+
+          case fun.(region, resource) do
+            {:ok, res} ->
+              case res.status_code do
+                200 -> :ok
+                _   -> show_error_message_and_code(res)
+              end
+            {:error, reason} ->
+              parse_http_error(reason)
+          end
+        end
+
+        defp cd_with_img(region, size, image, fun \\ &GCloudex.ComputeEngine.Client.insert_disk/3) do
+          epoch    = {{1970, 1, 1}, {0, 0, 0}} |> :calendar.datetime_to_gregorian_seconds
+          name     = "disk-#{epoch}" 
+          resource = %{"name" => name, "sizeGb" => size}
+
+          case fun.(region, resource) do
+            {:ok, res} ->
+              case res.status_code do
+                200 -> :ok
+                _   -> show_error_message_and_code(res)
+              end
+            {:error, reason} ->
+              parse_http_error(reason)
+          end
+        end
 
         def delete_disk(region, disk, fun \\ &GCloudex.ComputeEngine.Client.delete_disk/2) do
           case fun.(region, disk) do
@@ -240,13 +278,13 @@ if Code.ensure_loaded?(GCloudex) do
         #end
 
         def attach_disk(region, instance, disk, device_name, fun \\ &GCloudex.ComputeEngine.Client.attach_disk/3) do
-          source   = get_disk_self_link(region, disk) 
+          source   = get_disk_self_link(region, disk)
           resource = %{"source" => source, "deviceName" => device_name}
           case fun.(region, instance, resource) do
             {:ok, res} ->
               case res.status_code do
                 200 -> :ok
-                _   -> res#show_error_message_and_code(res) 
+                _   -> res#show_error_message_and_code(res)
               end
             {:error, reason} ->
               parse_http_error(reason)
@@ -257,7 +295,7 @@ if Code.ensure_loaded?(GCloudex) do
           case fun.(region, instance, disk) do
             {:ok, res} ->
               case res.status_code do
-                200 -> 
+                200 ->
                   show_error_message_and_code(res)
                   :ok
                 _   -> res |> show_error_message_and_code(:json)
@@ -273,21 +311,21 @@ if Code.ensure_loaded?(GCloudex) do
 
         def list_regions(fun \\ &GCloudex.ComputeEngine.Client.list_regions/1) do
           query_params = %{"fields" => "items/name, items/zones"}
-          case fun.(query_params) do 
+          case fun.(query_params) do
             {:ok, res} ->
               case res.status_code do
                 200 ->
                   res.body
-                  |> Poison.decode! 
-                  |> Map.get("items") 
+                  |> Poison.decode!
+                  |> Map.get("items")
                   |> Enum.map(
-                    fn map -> 
+                    fn map ->
                       {
-                        map["name"], Enum.map(map["zones"], 
-                          fn zone -> 
+                        map["name"], Enum.map(map["zones"],
+                          fn zone ->
                             zone |> String.split("/") |> List.last
                           end)
-                      } 
+                      }
                     end)
                 _   ->
                   res |> show_error_message_and_code
@@ -297,19 +335,19 @@ if Code.ensure_loaded?(GCloudex) do
           end
         end
 
-        def list_classes(region, fun \\ &list_machine_types/2) do 
+        def list_classes(region, fun \\ &list_machine_types/2) do
           query_params = %{"fields" => "items/name"}
           case fun.(region, query_params) do
-            {:ok , res} -> 
-              case res.status_code do 
+            {:ok , res} ->
+              case res.status_code do
                 200 ->
                   res.body
-                  |> Poison.decode! 
-                  |> Map.get("items") 
+                  |> Poison.decode!
+                  |> Map.get("items")
                   |> Enum.map(fn map -> map["name"] end)
 
-                _   -> 
-                  res |> show_error_message_and_code              
+                _   ->
+                  res |> show_error_message_and_code
               end
             {:error, reason} ->
               parse_http_error reason
@@ -320,12 +358,12 @@ if Code.ensure_loaded?(GCloudex) do
         ### Helpers ###
         ###############
 
-        defp check_for_nil(key, fun) do 
-          case key do 
+        defp check_for_nil(key, fun) do
+          case key do
             nil -> []
             _   -> Enum.map(key, fn x -> fun.(x) end)
           end
-        end        
+        end
 
         defp get_disk_self_link(region, disk, fun \\ &GCloudex.ComputeEngine.Client.get_disk/3) do
           fields = "selfLink"
