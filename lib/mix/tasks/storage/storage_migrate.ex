fd763601efb369b92c88ea67ffa67c728462d85f
Sasha Fonseca <sashaafm@gmail.com>
2016-06-22 11:19:07 +0100

    Solved the problems in the Storage.Migrate Mix Task.

--- lib/mix/tasks/storage/storage_migrate.ex
+++ lib/mix/tasks/storage/storage_migrate.ex
@@ -1,9 +1,5 @@
 defmodule Mix.Tasks.Nomad.Storage.Migrate do
   use Mix.Task
-  #case Application.get_env(:nomad, :cloud_to_migrate) do
-    #  :aws -> use Nomad.AWS.Storage, :aws
-    #:gcl -> use Nomad.GCL.Storage, :gcl
-    #end
   alias Nomad.TasksHelper, as: Helper
 
   @moduledoc"""
@@ -14,12 +10,17 @@ defmodule Mix.Tasks.Nomad.Storage.Migrate do
 
   @shortdoc"Migrate a storage from one service to another copying all the files and directories."
 
+  defp get_cloud_to_migrate, do: Application.get_env(:nomad, :cloud_to_migrate)
+
+  @callback upload(stor_dest :: binary, filename :: binary, file :: binary) :: :ok | binary
+  defdelegate upload(stor_dest, filename, file), to: Module.concat([Nomad, String.upcase(Atom.to_string(Application.get_env(:nomad, :cloud_to_migrate))), Storage]), as: :put_item
+
   def run(args) when length(args) == 2 do
     stor_origin = args |> Enum.fetch!(0)
     stor_dest   = args |> Enum.fetch!(1)
 
     Helper.start_apps_for_adapter(Helper.get_provider)
-    Helper.start_apps_for_adapter(Application.get_env(:nomad, :cloud_to_migrate))
+    Helper.start_apps_for_adapter(get_cloud_to_migrate)
 
     files = stor_origin
     |> Nomad.Storage.list_items
@@ -27,7 +28,7 @@ defmodule Mix.Tasks.Nomad.Storage.Migrate do
     for file <- files do
       :ok      = Nomad.Storage.get_item(stor_origin, file)
       filename = file |> String.split("/") |> List.last
-      # :ok      = put_item(stor_dest, filename, file)
+      :ok      = upload(stor_dest, filename, file)
       File.rm! filename
     end
   end
