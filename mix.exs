3711377fb06700fbb165f3396cc10bd8b160ff01
sasha.mojoodi <sasha.mojoodi@itrust.pt>
2016-09-02 15:40:27 +0100

    Removed exrm and updated TableRex.

--- mix.exs
+++ mix.exs
@@ -25,11 +25,10 @@ defmodule Nomad.Mixfile do
 
   defp deps do
     [
-      {:exrm,             github: "bitwalker/exrm"},
       {:credo,            "~> 0.3",  only: [:dev, :test]},
       {:ex_doc,           "~> 0.11", only: [:dev]},
       {:httpoison,        "~> 0.8.1"},
-      {:table_rex,        "~> 0.8.0"},
+      {:table_rex,        "~> 0.8.1"},
       {:friendly,         "~> 1.0.0"},
       {:goth,             "~> 0.1.2"},
       {:ex_aws,           github: "sashaafm/ex_aws", branch: "merge-rds-and-ec2-for-testing"},
